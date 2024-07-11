91119704626062af047bb3900a7458144b0982f2
Jayadeep8143 <jayadeepjay807@gmail.com>
2024-07-11 16:21:31 +0530

    welcome

--- src/mycontactapp/Welcome.java
+++ src/mycontactapp/Welcome.java
@@ -3,8 +3,9 @@ package mycontactapp;
 public class Welcome {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to GitHub");
 		// TODO Auto-generated method stub
-
+      System.out.println("Git tool");
 	}
 
 }
