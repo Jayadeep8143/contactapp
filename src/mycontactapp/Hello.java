e4b9c469b04ac4bb3a911df3b9d5571cf9e0f0e6
Jayadeep8143 <jayadeepjay807@gmail.com>
2024-07-11 15:41:39 +0530

    commit

---
+++ src/mycontactapp/Hello.java
@@ -0,0 +1,11 @@
+package mycontactapp;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello ...everybody");
+		// TODO Auto-generated method stub
+
+	}
+
+}
